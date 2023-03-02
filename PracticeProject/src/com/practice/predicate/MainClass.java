043e488a7ba384298163e0768a497123383610f2
Swapnil_Gawali <gawaliswapnil1@gmail.com>
2023-03-02 11:49:24 +0530

    For predicates initial

--- PracticeProject/src/com/practice/predicate/MainClass.java
+++ PracticeProject/src/com/practice/predicate/MainClass.java
@@ -12,11 +12,24 @@ public class MainClass {
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
+        // Example 1 not greater than 5
         Predicate<Integer> notGreaterThanFive = x -> x < 5;
 
         List<Integer> collect = list.stream().filter(notGreaterThanFive).collect(Collectors.toList());
         System.out.println(collect);
 
+        //and condition in predicates
+
+        Predicate<Integer> notLessThan3 = x -> x > 3;
+
+        List<Integer> andFiltered = list.stream().filter(notGreaterThanFive.and(notLessThan3)).collect(Collectors.toList());
+        System.out.println(andFiltered);
+
+        // predicate with Negate
+        Predicate<String> startWithA=x -> x.startsWith("A");
+        List<String> charString=Arrays.asList("A","AA","AAA","B","BB");
+        List<String> notACharList = charString.stream().filter(startWithA.negate()).collect(Collectors.toList());
+        System.out.println(notACharList);
     }
 }
 
